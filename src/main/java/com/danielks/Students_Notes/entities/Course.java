f7efa90558704563e287027615a5c34528738fcb
dschifel <danielschifelbain@gmail.com>
2024-09-09 19:28:14 -0300

    Created Course Entity

---
+++ src/main/java/com/danielks/Students_Notes/entities/Course.java
@@ -0,0 +1,75 @@
+package com.danielks.Students_Notes.entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+    private int maxStudents;
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Student> students;
+
+    public Course() {
+    }
+
+    public Course(UUID id, String name, int maxStudents, List<Student> students) {
+        this.id = id;
+        this.name = name;
+        this.maxStudents = maxStudents;
+        this.students = students;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxStudents() {
+        return maxStudents;
+    }
+
+    public void setMaxStudents(int maxStudents) {
+        this.maxStudents = maxStudents;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id.equals(course.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
