e4fb6dd00ce9191eba306fd1a27070b51eca2c85
dschifel <danielschifelbain@gmail.com>
2024-09-10 17:51:17 -0300

    Created Student DTO

---
+++ src/main/java/com/danielks/Students_Notes/entities/dtos/StudentDTO.java
@@ -0,0 +1,17 @@
+package com.danielks.Students_Notes.entities.dtos;
+
+import com.danielks.Students_Notes.entities.Course;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+import java.util.UUID;
+
+public record StudentDTO(
+        UUID id,
+        @NotEmpty(message = "name is blank") String name,
+        @NotEmpty(message = "age is blank") int age,
+        @NotNull(message = "course is blank") Course course,
+        List<Double> notes
+        ) {
+}
