736b862244282a504c6c07d5e3147caee0800626
DanielKriegerS <danielschifelbain@gmail.com>
2024-09-13 18:57:15 -0300

    Created student service

---
+++ src/main/java/com/danielks/Students_Notes/services/StudentService.java
@@ -0,0 +1,36 @@
+package com.danielks.Students_Notes.services;
+
+import com.danielks.Students_Notes.entities.Student;
+import com.danielks.Students_Notes.entities.dtos.StudentDTO;
+import com.danielks.Students_Notes.entities.mappers.StudentMapper;
+import com.danielks.Students_Notes.repositories.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+    private final StudentMapper studentMapper;
+
+    public StudentService(StudentRepository studentRepository, StudentMapper studentMapper) {
+        this.studentRepository = studentRepository;
+        this.studentMapper = studentMapper;
+    }
+
+    public List<StudentDTO> getAllStudents() {
+        List<Student> students = studentRepository.findAll();
+        return students.stream()
+                .map(studentMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    public StudentDTO getStudentById(UUID id) {
+        return studentRepository.findById(id)
+                .map(studentMapper::toDto)
+                .orElse(null);
+    }
+}
