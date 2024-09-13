f687a0287dc0af4559a59aa887573da762dd3c46
DanielKriegerS <danielschifelbain@gmail.com>
2024-09-12 21:20:22 -0300

    Created course controller + gets basic methods

---
+++ src/main/java/com/danielks/Students_Notes/controllers/CourseController.java
@@ -0,0 +1,35 @@
+package com.danielks.Students_Notes.controllers;
+
+import com.danielks.Students_Notes.entities.dtos.CourseDTO;
+import com.danielks.Students_Notes.services.CourseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable UUID id) {
+        CourseDTO courseDTO = courseService.getCourseById(id);
+        return ResponseEntity.ok(courseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseDTO>> getAllCourses() {
+        List<CourseDTO> courses = courseService.getAllCourses();
+        return ResponseEntity.ok(courses);
+    }
+}
