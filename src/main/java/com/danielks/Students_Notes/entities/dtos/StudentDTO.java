ca5a2920c88cab367d09f8fe166bd92ca8d50fde
dschifel <danielschifelbain@gmail.com>
2024-09-26 20:03:13 -0300

    Fixed: DTO not receive directly the course in creation

--- src/main/java/com/danielks/Students_Notes/entities/dtos/StudentDTO.java
+++ src/main/java/com/danielks/Students_Notes/entities/dtos/StudentDTO.java
@@ -1,8 +1,6 @@
 package com.danielks.Students_Notes.entities.dtos;
 
-import com.danielks.Students_Notes.entities.Course;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,7 +9,6 @@ public record StudentDTO(
         UUID id,
         @NotEmpty(message = "name is blank") String name,
         @NotEmpty(message = "age is blank") int age,
-        @NotNull(message = "course is blank") UUID courseId,
         List<Double> notes
         ) {
 }
