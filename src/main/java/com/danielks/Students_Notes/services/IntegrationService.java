c1301dc9202ff5d559595bfe3e0bc0a2db1526cb
dschifel <danielschifelbain@gmail.com>
2024-10-02 18:58:44 -0300

    New + Feat: Created integration service + implemented enroll in course method

---
+++ src/main/java/com/danielks/Students_Notes/services/IntegrationService.java
@@ -0,0 +1,36 @@
+package com.danielks.Students_Notes.services;
+
+import com.danielks.Students_Notes.entities.Course;
+import com.danielks.Students_Notes.entities.Student;
+import com.danielks.Students_Notes.entities.dtos.StudentDTO;
+import com.danielks.Students_Notes.entities.mappers.StudentMapper;
+import com.danielks.Students_Notes.repositories.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class IntegrationService {
+
+    private final StudentRepository studentRepository;
+    private final ValidationService validator;
+    private final StudentMapper studentMapper;
+
+    public IntegrationService(StudentRepository studentRepository, ValidationService validator, StudentMapper studentMapper) {
+        this.studentRepository = studentRepository;
+        this.validator = validator;
+        this.studentMapper = studentMapper;
+    }
+
+    public StudentDTO enrollStudentInCourse(UUID studentId, UUID courseId) {
+        Student student = validator.validateStudent(studentId);
+        Course course = validator.validateCourse(courseId);
+
+        validator.validateEnroll(student, course);
+
+        student.setCourse(course);
+        studentRepository.save(student);
+
+        return studentMapper.toDto(student);
+    }
+}
